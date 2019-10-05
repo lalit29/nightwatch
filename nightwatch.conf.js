0ecc3574372a9bc72440d9bb015516ecb473ce70
John Doe <lalitsukhwal296@gmail.com>
2019-10-05 17:54:20 +0530

    initial commit

---
+++ nightwatch.conf.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+module.exports = {
+    'src_folders': ['tests'],
+    'page_objects_path': ['page-objects'],
+
+    'webdriver': {
+        'start_process': true,
+        'server_path': require('chromedriver').path,
+        // 'server_path': 'C:\\Users\\ASUS\\Desktop\\javascript\\POC\\drivers\\chromedriver.exe',
+        'port': 9515
+    },
+
+    'test_settings': {
+        'default': {
+            'screenshots': {
+                'enabled': true,
+                'on_failure': true,
+                'on_error': true,
+                'path': 'tests_output/screenshots'
+            },
+            'desiredCapabilities': {
+                'browserName': 'chrome',
+                'chromeOptions': {
+                    'args': ['--headless','--window-size=1860,1200','--no-sandbox']
+                }
+            }
+        }
+    }
+};
---
+++ tests/firstTest.js
@@ -0,0 +1,9 @@
+module.exports = {
+    '@disabled': true,
+    'My first test case'(browser) {
+        browser
+            .url('https://news.ycombinator.com/')
+            .waitForElementVisible('.hnname')
+            .assert.containsText('.hnname', 'Hacker News');
+    }
+};
